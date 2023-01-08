a57d9ba401bdbf9243b8749ac5e447a33fde03af
Damianoo00 <dplaskowicki@simp-consulting.com>
2023-01-08 10:24:41 +0100

    CMake libs

--- Inc/RegisterClass.h
+++ Inc/RegisterClass.h
@@ -11,14 +11,10 @@ namespace max17851
         uint8_t iReadAddress{0};
         uint8_t iRegister{0};
         Register(const uint8_t writeAddress, const uint8_t readAddress) : iWriteAddress(writeAddress), iReadAddress(readAddress), iRegister(0) {}
+        static Register &Instance();
 
-    public:
+    protected:
         uint8_t get(uint8_t startBit, uint8_t lengthBitSet);
         void set(uint8_t startBit, uint8_t lenghBitset, uint8_t val);
-
-    public:
-        void sendRegister();
-        void reciveRegister();
-        static Register &Instance();
     };
 }
