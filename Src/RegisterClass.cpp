126be9fb3320a9b4b0af754400b7a34e1d8e617c
damianoo00 <dplaskowicki@simp-consulting.com>
2023-01-31 13:42:08 +0100

    fix get bit array

--- Src/RegisterClass.cpp
+++ Src/RegisterClass.cpp
@@ -5,7 +5,7 @@ namespace max17851
     uint8_t Register::get(uint8_t startBit, uint8_t lengthBitSet)
     {
         const uint8_t mask = ((uint8_t)pow(2, lengthBitSet) - 1) << startBit;
-        return iRegister & mask >> startBit;
+        return (iRegister & mask) >> startBit;
     }
     void Register::set(uint8_t startBit, uint8_t lenghBitset, uint8_t val)
     {
